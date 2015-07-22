022f188f11648dbe4b845ba334731513b5b6cb15
Vernon O'Reilly <vernono@LPC03NQDM.ibm.com>
2015-07-23 11:15:45 +1200

    A generic perl header banner file

---
+++ perlheader.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+############################################################################### 
+# hello.pl
+#  Displays a warm greeting.
+#  
+# AUTHOR
+#  Vernon O' Reilly
+# DATE
+#  23 July 2015
+# VERSION
+#  1.0
+# PURPOSE
+#  Enter purpose here
+# USAGE
+#  perl hello.pl
+# LICENSE
+#  You may copy and redistribute this program as you see fit, with no
+#  restrictions.
+# WARRANTY
+#  This program comes with NO warranty, real or implied.
+############################################################################### 
+
+use strict;
+use warnings;
+use v5.10;
+use Data::Dumper;
+
